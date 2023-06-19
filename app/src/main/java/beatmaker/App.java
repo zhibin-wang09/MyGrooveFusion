520d0045eb740fba007c5b6324401903a5f50d97
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-06-18 22:43:29 -0400

    Change from simply invoking UI to starting a new thread due to the thread safety concerns of the Swing library

--- app/src/main/java/beatmaker/App.java
+++ app/src/main/java/beatmaker/App.java
@@ -4,11 +4,12 @@
 package beatmaker;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        javax.swing.SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                UI ui = new UI();
+                ui.show();
+            }
+        });
     }
 }
