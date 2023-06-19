2da99b1f3f72edd0f0d847eaa59919702c12d864
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-06-18 22:42:17 -0400

    The UI class is able to display the list of panels which includes buttons and a label.

--- app/src/main/java/beatmaker/UI.java
+++ app/src/main/java/beatmaker/UI.java
@@ -1,16 +1,69 @@
 package beatmaker;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.WindowConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.Container;
 
+import java.util.ArrayList;
+
+/**
+ * A class to manipulate the UI for the beat maker application
+ */
 public class UI {
-    JFrame frame;
+    JFrame frame; // the base frame that the program uses
+    BeatPlayer beatPlayer; // the music player that the program will be interacting with
+
+    /**
+     * This constructor sets the default setting of the <code>frame</code>
+    */
     public UI(){
         frame = new JFrame("Beat Maker");
-        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE); // makes the default action of closing frame is to do nothing
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // change default behavior to exit on close
+        frame.setResizable(false);
+        frame.setSize(900,600);
+        frame.setIconImage(new ImageIcon("../resources/images/beats.png").getImage());
+        frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        beatPlayer = new BeatPlayer();
     }
 
+    /**
+     * This method will display the frame to the user
+    */
     public void show(){
-        
+        ArrayList<JPanel> panels = generatePanels();
+        Container rootPane = frame.getContentPane(); 
+        for(JPanel p : panels){
+            rootPane.add(p);
+        }
         frame.setVisible(true); // JFrame is initially invisible, so set visible
+    }
+
+    /**
+     * This function generate the list of audio players for each audio that 
+     * exist in <code>beatPlayer</code>'s list of audios.
+     * 
+     * @return a list of JPanels that can be implemented to <code>frame</code>
+     */
+    private ArrayList<JPanel> generatePanels(){
+        ArrayList<JPanel> panels = new ArrayList<>();
+        for(Audio audio : beatPlayer.getAudios()){
+            JPanel panel = new JPanel();
+            panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+            JButton play = new JButton("PLAY", null);
+            JButton stop = new JButton("STOP", null);
+            JButton resume = new JButton("RESUME", null);
+            JLabel label = new JLabel(audio.getName() );
+            panel.add(play);
+            panel.add(stop);
+            panel.add(resume);
+            panel.add(label);
+            panels.add(panel);
+        }
+        return panels;
     }
 }
