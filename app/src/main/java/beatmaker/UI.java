fb86bdf5168635422389f82e0154644a8b524a05
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-06-21 22:26:31 -0400

    Add a description popup when the application is opened

--- app/src/main/java/beatmaker/UI.java
+++ app/src/main/java/beatmaker/UI.java
@@ -1,13 +1,17 @@
 package beatmaker;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.WindowConstants;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 
+import java.awt.Component;
 import java.awt.Container;
 
 /**
@@ -28,7 +32,7 @@ public class UI {
         frame.setResizable(false);
         frame.setSize(900,600);
         frame.setLocationRelativeTo(null);
-        frame.setIconImage(new ImageIcon("../resources/images/beats.png").getImage());
+        frame.setIconImage(new ImageIcon("./app/src/main/resources/images/beats.png").getImage());
         rootPane = frame.getContentPane();
         beatPlayer = new BeatPlayer();
     }
@@ -39,6 +43,7 @@ public class UI {
     public void show(){
         addPanels();
         frame.setVisible(true); // JFrame is initially invisible, so set visible
+        showGuide();
     }
 
     /**
@@ -53,6 +58,7 @@ public class UI {
         JPanel libraryBase = new JPanel();
         for(Audio audio : beatPlayer.getAudios()){ // for every audio that exist create a panel
             libraryBase.add(audio); // root pane add instead of JFrame
+            audio.setAlignmentX(Component.LEFT_ALIGNMENT);
         }
 
         libraryBase.setLayout(new BoxLayout(libraryBase, BoxLayout.Y_AXIS)); // the libraryBase will display the panels
@@ -68,7 +74,7 @@ public class UI {
 
 
         JButton done = new JButton("Done");
-        done.addActionListener(e -> BeatPlayer.joinClips());
+        done.addActionListener(e -> produce());
 
         product.add(clear);
         product.add(upload);
@@ -76,5 +82,38 @@ public class UI {
         product.add(productName);
         base.add(product);
         rootPane.add(base);
+    }
+
+    public void produce(){
+        JFileChooser fileChooser = new JFileChooser("./app/src/main/resources/production");
+        fileChooser.setFileFilter(new FileNameExtensionFilter("WAVE FILES", "wav", "wave"));
+        int response = fileChooser.showSaveDialog(null);
+        if(response == JFileChooser.APPROVE_OPTION){
+            boolean status = BeatPlayer.joinClips(fileChooser.getSelectedFile().getAbsolutePath());
+            if(status){
+                JOptionPane.showMessageDialog(null,"The file has been added successfully!");
+            }
+        }
+    }
+
+    public void showGuide(){
+        /* <a href="https://www.flaticon.com/free-icons/midi" title="midi icons">Midi icons created by Freepik - Flaticon</a> */
+        JOptionPane.showMessageDialog(null, 
+         """
+            This is a brief guide on how to use this program.\n
+            \t1. The left most button is the play button. Pressing the button will always starts at the beginning of the audio.\n
+            \t2. The second button to left is the pause button, it pauses the audio.\n
+            \t3. Then it's the resume button, it will start where it left off.\n
+            \t4. Lastly, it's the add button which clips piece of the audio and uses it to make new beats.\n
+            \t5. The slider is used to precisely choose where you want the beat to start. It can also be used to choose the starting point of the clip.\n
+            \t6. The \"clip\" means the range of audio/beat that is going to be extracted and added to the result track.\n
+            \tThis \"clip\" starts at the slider position after you moved it and stops when you hit stop and add.\n
+            \t7. The small panel in the bottom is used to control the intermediate steps of the final audio/beat build.\n
+            \t\ta. The first button clears the \"clips\" that have been added\n
+            \t\tb. The upload button uploads the button to a website (You can configure this website by heading to the web folder).\n
+            \t\tc. The finish button builds the final production audio/beat. you will be prompt a window to locate where to store the production.\n
+            You can always come back to this panel for information. \n.
+            HAVE FUN MESSING AROUND!.\n
+         """, "Brief Guide" ,JOptionPane.INFORMATION_MESSAGE, new ImageIcon("./app/src/main/resources/images/midi.png"));
     }
 }
