926af53133ffb4cedac20c81942c7972a62ead67
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-07-07 15:17:05 -0400

    Written unit test for Audio class

--- app/src/test/java/beatmaker/AudioTest.java
+++ app/src/test/java/beatmaker/AudioTest.java
@@ -2,7 +2,6 @@ package beatmaker;
 
 import org.junit.jupiter.api.Test;
 import java.io.File;
-import java.sql.SQLOutput;
 import java.util.Objects;
 import javax.sound.sampled.Clip;
 
@@ -19,18 +18,14 @@ class AudioTest {
             assertNotEquals(null, f.listFiles());
             for(final File audio : Objects.requireNonNull(f.listFiles())){
                 Audio a = new Audio(audio.getName(), audio);
-                a.play();
+                a.init();
                 Clip c = a.getClip();
-                assert c != null;
+                c.start();
                 assertTrue(c.isOpen());
                 c.stop();
                 c.close();
             }
         }
-    }
-
-    @Test
-    void resume() {
     }
 
     @Test
@@ -42,17 +37,45 @@ class AudioTest {
             assertNotEquals(null, f.listFiles());
             for(final File audio : Objects.requireNonNull(f.listFiles())){
                 Audio a = new Audio(audio.getName(), audio);
-
+                a.init();
+                Clip c = a.getClip();
+                c.start();
+                c.stop();;
+                assertFalse(c.isRunning());
+                c.close();
             }
         }
     }
 
     @Test
     void getName() {
+        File[] direc = libraryFolder.listFiles();
+        assert direc != null;
+        for(File f : direc){
+            if(f.getName().equals(".DS_Store")) continue;
+            for(File audio : Objects.requireNonNull(f.listFiles())){
+                Audio a = new Audio(audio.getName(), audio);
+                assertEquals(a.getName(), audio.getName());
+            }
+        }
     }
 
     @Test
     void close() {
+        File[] direc = libraryFolder.listFiles();
+        assert direc != null;
+        for(final File f : direc){
+            if(f.getName().equals(".DS_Store")) continue;
+            assertNotEquals(null, f.listFiles());
+            for(final File audio : Objects.requireNonNull(f.listFiles())){
+                Audio a = new Audio(audio.getName(), audio);
+                a.init();
+                Clip c = a.getClip();
+                assertTrue(c.isOpen());
+                c.close();
+                assertFalse(c.isActive());
+            }
+        }
     }
 
 }
